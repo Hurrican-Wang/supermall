9aca922b3d5a3d8ab9be4971f67215b13d790057
Hurrican-Wang <563036847@qq.com>
2021-03-23 11:37:05 +0800

    test

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,45 @@
 <template>
   <div>
-    首页
+    <nav-bar class="home-nav">
+      <template #center>
+        购物街
+      </template>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './homeswiper/HomeSwiper'
+  import {getHomeMultidata} from 'network/home'
+  
+  
   export default {
-    name: 'Home'
-    
+    name: 'Home',
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+        // console.log(res.data);
+      })
+    }
   }
 </script>
 
 <style scope>
-  
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
---
+++ src/views/home/homeswiper/HomeSwiper.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-item v-for="item in banners">
+        <template #default>
+          <a :href="item.link">
+            <img :src="item.image" alt="">
+          </a>
+        </template>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: 'HomeSwiper',
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    props: {
+      banners: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+    
+  }
+</script>
+
+<style scope>
+  
+</style>
