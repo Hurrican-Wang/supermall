ecd6c10c16d5e879a64b4970d2f8607b43b8ab1d
Hurrican-Wang <563036847@qq.com>
2021-03-23 19:39:51 +0800

    test

--- src/components/common/Tab-Bar/TabBar.vue
+++ src/components/common/Tab-Bar/TabBar.vue
@@ -13,12 +13,12 @@
 <style scope>
   #tab-bar {
     display: flex;
-    background-color: #f6f6f6;
     position: fixed;
     left: 0;
     right: 0;
     bottom: 0;
     box-shadow: 0 -2px 1px rgba(100, 100, 100, .15);
+    background-color: #f6f6f6;
   }
  
 </style>
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,16 +1,82 @@
 <template>
-  <div>
-    分类
+  <div class="wrapper" ref="wrapper">
+    <ul class="content">
+      <li>展示1</li>
+      <li>展示2</li>
+      <li>展示3</li>
+      <li>展示4</li>
+      <li>展示5</li>
+      <li>展示6</li>
+      <li>展示7</li>
+      <li>展示8</li>
+      <li>展示9</li>
+      <li>展示10</li>
+      <li>展示11</li>
+      <li>展示12</li>
+      <li>展示13</li>
+      <li>展示14</li>
+      <li>展示15</li>
+      <li>展示16</li>
+      <li>展示17</li>
+      <li>展示18</li>
+      <li>展示19</li>
+      <li>展示20</li>
+      <li>展示21</li>
+      <li>展示22</li>
+      <li>展示23</li>
+      <li>展示24</li>
+      <li>展示25</li>
+      <li>展示26</li>
+      <li>展示27</li>
+      <li>展示28</li>
+      <li>展示29</li>
+      <li>展示30</li>
+      <li>展示31</li>
+      <li>展示32</li>
+      <li>展示33</li>
+      <li>展示34</li>
+      <li>展示35</li>
+      <li>展示36</li>
+      <li>展示37</li>
+      <li>展示38</li>
+      <li>展示39</li>
+      <li>展示40</li>
+      <li>展示41</li>
+      <li>展示42</li>
+      <li>展示43</li>
+      <li>展示44</li>
+      <li>展示45</li>
+      <li>展示46</li>
+      <li>展示47</li>
+      <li>展示48</li>
+      <li>展示49</li>
+      <li>展示50</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Category'
-    
+import BScroll from "better-scroll";
+
+export default {
+  name: 'Category',
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+   
+    })
   }
+}
 </script>
 
 <style scope>
-  
+.wrapper {
+  height: 150px;
+  background-color: red;
+  overflow: hidden;
+}
 </style>
